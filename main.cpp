dac1e384c0cd2366e86aeb8385e3a84b5e11617c
itsvoit <wojciech.skrzypiec2@gmail.com>
2022-10-13 17:04:22 +0200

    Add tests for CTreeDynamic

--- main.cpp
+++ main.cpp
@@ -4,8 +4,9 @@
 
 #include <iostream>
 #include "CTreeStatic.h"
+#include "CTreeDynamic.h"
 
-void v_tree_test()
+void v_tree_static_test()
 {
 	CNodeStatic c_root;
 	c_root.vAddNewChild();
@@ -23,12 +24,37 @@ void v_tree_test()
 
 	std::cout << "Print up from 11:";
 	c_root.pcGetChild(0)->pcGetChild(0)->vPrintUp();
-}// void v_tree_test()
 
+}// void v_tree_static_test()
+
+
+void v_tree_dynamic_test(){
+	CTreeDynamic c_tree;
+	c_tree.pcGetRoot()->vAddNewChild();
+	c_tree.pcGetRoot()->vAddNewChild();
+	c_tree.pcGetRoot()->pcGetChild(0)->vSetValue(1);
+	c_tree.pcGetRoot()->pcGetChild(1)->vSetValue(2);
+	c_tree.pcGetRoot()->pcGetChild(0)->vAddNewChild();
+	c_tree.pcGetRoot()->pcGetChild(0)->vAddNewChild();
+	c_tree.pcGetRoot()->pcGetChild(0)->pcGetChild(0)->vSetValue(11);
+	c_tree.pcGetRoot()->pcGetChild(0)->pcGetChild(1)->vSetValue(12);
+	c_tree.pcGetRoot()->pcGetChild(1)->vAddNewChild();
+	c_tree.pcGetRoot()->pcGetChild(1)->vAddNewChild();
+	c_tree.pcGetRoot()->pcGetChild(1)->pcGetChild(0)->vSetValue(21);
+	c_tree.pcGetRoot()->pcGetChild(1)->pcGetChild(1)->vSetValue(22);
+
+	std::cout << "Print tree:\n";
+	c_tree.vPrintTree();
+
+
+
+}// v_tree_dynamic_test()
 
 int main(){
 
-	v_tree_test();
+	v_tree_static_test();
+
+	v_tree_dynamic_test();
 
 	return 0;
 }
