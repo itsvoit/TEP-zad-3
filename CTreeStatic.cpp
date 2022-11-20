363918bf15b4843345f5082acb6b80a1dd26573a
itsvoit <wojciech.skrzypiec2@gmail.com>
2022-11-20 23:01:30 +0100

    Add root doesn't exist checks Add bMoveSubTree     - update parents after every operation Change printing to print tree structure instead of list

--- CTreeStatic.cpp
+++ CTreeStatic.cpp
@@ -3,12 +3,17 @@
 //
 
 #include "CTreeStatic.h"
+#include <algorithm>
+
+// --------------------------------
+//	class CNodeStatic
+
 
 void CNodeStatic::vAddNewChild() {
-	std::cout << "Add new child\n";
-	CNodeStatic *pcTempNode = new CNodeStatic;
-	pcTempNode->pc_parent_node = this;
-	v_children.push_back(*pcTempNode);
+//	std::cout << "Add new child\n";
+	CNodeStatic pcTempNode;
+	v_children.push_back(pcTempNode);
+	vUpdateParentsBelow();
 }
 
 CNodeStatic *CNodeStatic::pcGetChild(int iChildOffset) {
@@ -18,15 +23,23 @@ CNodeStatic *CNodeStatic::pcGetChild(int iChildOffset) {
 	return &v_children[iChildOffset];
 }
 
-void CNodeStatic::vPrintAllBelow() {
+void CNodeStatic::vPrintAllBelow(int level) {
+	for (int i = 0; i < level; ++i) {
+		std::cout << "  ";
+	}
+	vPrint();
+	std::cout << "\n";
 	for (int i = 0; i < v_children.size(); ++i) {
-		v_children[i].vPrint();
-		v_children[i].vPrintAllBelow();
+		v_children[i].vPrintAllBelow(level+1);
 	}
 }
 
+void CNodeStatic::vPrintAllBelow() {
+	vPrintAllBelow(0);
+}
+
 CNodeStatic::~CNodeStatic() {
-	std::cout << "Deleting node with value: " << i_val << std::endl;
+//	std::cout << "Deleting node with value: " << i_val << std::endl;
 }
 
 void CNodeStatic::vPrintUp() {
@@ -37,15 +50,60 @@ void CNodeStatic::vPrintUp() {
 		std::cout << std::endl;
 }
 
+void CNodeStatic::vUpdateParentsBelow(){
+	for (std::vector<CNodeStatic>::iterator it = v_children.begin(); it != v_children.end(); ++it){
+		it -> pc_parent_node = this;
+		if (it -> v_children.size() != 0) it->vUpdateParentsBelow();
+	}
+}
+
+// --------------------------------
+//	class CTreeStatic
+
 CTreeStatic::CTreeStatic() {
+	b_root_exists = true;
 }
 
 CTreeStatic::~CTreeStatic() {
+}
 
+CNodeStatic* CTreeStatic::pcGetRoot() {
+	if (!b_root_exists) return NULL;
+	return(&c_root);
 }
 
 void CTreeStatic::vPrintTree() {
-	for (int i = 0; i < c_root.iGetChildrenNumber(); ++i) {
-		c_root.pcGetChild(i)->vPrintAllBelow();
+	if (!b_root_exists) {
+		std::cout << "Root does not exist";
+		return;
 	}
+	c_root.vPrintAllBelow();
+}
+
+bool CTreeStatic::bMoveSubTree(CNodeStatic *pcParentNode, CNodeStatic *pcNewChildNode){
+	if (pcParentNode == NULL || pcNewChildNode == NULL) return false;
+
+	// move newChildNode to parentNode
+	// fix parents for every node (even already existing because vector can extend)
+	pcParentNode -> v_children.push_back(*pcNewChildNode);
+	pcParentNode->vUpdateParentsBelow();
+	
+	// remove newChildNode from its previous parent
+	// update parent nodes 
+	if (pcNewChildNode == &c_root){
+		b_root_exists = false;
+		return true;
+	}
+
+	std::vector<CNodeStatic> children = pcNewChildNode -> pc_parent_node -> v_children;
+	for (std::vector<CNodeStatic>::iterator it = children.begin(); it != children.end(); ++it){
+		if (&*it == pcNewChildNode) {
+			children.erase(it);
+			break;
+		}
+	}
+
+	pcNewChildNode->pc_parent_node->vUpdateParentsBelow();
+	
+	return true;
 }
--- CTreeStatic.h
+++ CTreeStatic.h
@@ -8,45 +8,44 @@
 #include <iostream>
 #include <vector>
 
+class CTreeStatic;
+
 class CNodeStatic{
 public:
-	CNodeStatic() {
-		i_val = 0;
-		pc_parent_node = NULL;
-	};
+	CNodeStatic() { i_val = 0; pc_parent_node = NULL;};
 	~CNodeStatic();
 
-	void vSetValue(int iNewVal) {
-		i_val = iNewVal;
-	};
-	int iGetChildrenNumber() {
-		return(v_children.size());
-	};
+	void vSetValue(int iNewVal) { i_val = iNewVal; };
+	int iGetChildrenNumber() { return(v_children.size()); };
 	void vAddNewChild();
 	CNodeStatic *pcGetChild(int iChildOffset);
-	void vPrint() {
-		std::cout << " " << i_val;
-	};
+	void vPrint() { std::cout << " " << i_val; };
 	void vPrintAllBelow();
 	void vPrintUp();
 
+	friend class CTreeStatic;
+
 private:
+	void vPrintAllBelow(int level);
 	std::vector<CNodeStatic> v_children;
 	CNodeStatic *pc_parent_node;
 	int i_val;
+	void vUpdateParentsBelow();
 };
 
 class CTreeStatic {
 public:
 	CTreeStatic();
 	~CTreeStatic();
-	CNodeStatic *pcGetRoot() {
-		return(&c_root);
-	}
+	CNodeStatic *pcGetRoot();
 	void vPrintTree();
+	bool bMoveSubTree(CNodeStatic *pcParentNode, CNodeStatic *pcNewChildNode);
+	
 private:
+	bool b_root_exists;
 	CNodeStatic c_root;
 };
+
 
 
 #endif //TEP_ZAD_3_CTREESTATIC_H
